a98831200a846f488b814b5c375998ff01c7f682
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-01-07 10:08:37 +0100

    Create modelUke1.js

---
+++ modelUke1.js
@@ -0,0 +1,39 @@
+const model = {
+    logInPage: {
+        adminUser: [
+            {userName: 'team4', isPasswordProtected: true},
+        ],
+        adminPwd: 'Getacademy',
+
+        normalUser: [
+            {userName: '', isPasswordProtected: false},
+        ],    
+    },
+
+    newPollPage: {
+        polls: [{
+            newQuestion: "",
+            multipleChoice: false,
+            answers: [],
+            pollDate: [{
+                dateCreated: [],
+                expireDate: [],
+            }]
+        }],     
+    },
+    
+    surveyPage: {
+        userAnswers: [{
+            answerCount: [],
+            answerPercentage: [],
+        }],
+        endPoll: '',
+    },
+
+    pollPageOverview: {
+            searchPoll: '',
+            endPoll: '',
+            addNewPoll: '',
+            choosePreviusPoll: '',
+    },
+}
